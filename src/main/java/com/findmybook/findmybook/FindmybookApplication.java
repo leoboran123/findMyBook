8abd7bda891be37bfdb78a9895a922e95fecb8d8
Erdem Boran Bad <erdem_boran_badd@hotmail.com>
2025-01-10 21:18:38 +0300

    Project initialized

---
+++ src/main/java/com/findmybook/findmybook/FindmybookApplication.java
@@ -0,0 +1,13 @@
+package com.findmybook.findmybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findmybook/findmybook/FindmybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmybook.findmybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
